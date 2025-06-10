145b3844b3f35d7918468bfe2d066325913f92d2
AndrewK <ajk345@cornell.edu>
2025-06-10 16:11:53 +0900

    working on bidirectional variants

--- SOH/CNN.py
+++ SOH/CNN.py
@@ -58,10 +58,50 @@ class LSTM_CNN(helper.base):
         return out
 
 
+# Class for BiLSTM_CNN
+class BiLSTM_CNN(helper.base):
+    # Constructor for the CNN BiLSTM
+    def __init__(self, num_features, conv_filters, kernel_size, hidden_units=16, num_layers=1, dropout=0.1):
+        super().__init__()
+        self.hidden = None
+        self.control = None
+        self.hidden_units = hidden_units
+        self.num_layers = num_layers
+
+        self.cnn = nn.Conv1d(
+            in_channels=num_features,
+            out_channels=conv_filters,
+            kernel_size=kernel_size,
+            padding="same"
+        )
+
+        self.lstm = nn.LSTM(
+            input_size=conv_filters,
+            hidden_size=hidden_units,
+            batch_first=True,
+            num_layers=num_layers,
+            dropout=dropout,
+            bidirectional=True
+        )
+
+        self.linear = nn.Linear(in_features=hidden_units, out_features=1)
+        self.relu = nn.ReLU()
+
+    # Defines forward propagation for each batch
+    def forward(self, inputX):
+        raise NotImplementedError
+
+    # Autoregression portion of the LSTM
+    # For future reference should maybe have to change for when you put in previous SOH into input
+    def predict(self, inputX):
+        raise NotImplementedError
+
+
 # Defined the training loop
 if __name__ == "__main__":
-    df_train, _, _ = helper.get_dataset("data/battery_log_processed.csv")
+    torch.manual_seed(1)
+    df_train, _, _ = helper.get_dataset("data/training/battery_log_processed.csv")
     df_train_loader = helper.get_loaders(df_train)
 
     LSTM_CNN_model = LSTM_CNN(len(list(df_train.columns.difference(["SOH[%]"]))), conv_filters=64, kernel_size=3)
-    helper.trainer(df_train_loader, LSTM_CNN_model, "loss2.csv")
+    helper.trainer(df_train_loader, LSTM_CNN_model, "data/losses/loss2.csv")
--- SOH/LSTM.py
+++ SOH/LSTM.py
@@ -44,10 +44,43 @@ class LSTM(nn.Module):
         return out
 
 
+# BiLSTM class
+class BiLSTM(nn.Module):
+    # Constructor for the BiDirectional LSTM
+    def __init__(self, num_features, hidden_units=16, num_layers=1, dropout=0.1):
+        super().__init__()
+        self.hidden = None
+        self.control = None
+        self.hidden_units = hidden_units
+        self.num_layers = num_layers
+
+        self.lstm = nn.LSTM(
+            input_size=num_features,
+            hidden_size=hidden_units,
+            batch_first=True,
+            num_layers=num_layers,
+            dropout=dropout,
+            bidirectional=True,
+        )
+
+        self.linear = nn.Linear(in_features=hidden_units, out_features=1)
+        self.relu = nn.ReLU()
+
+    # Defines forward propagation for each batch
+    def forward(self, inputX):
+        raise NotImplementedError
+
+    # Autoregression portion of the LSTM
+    # For future reference should maybe have to change for when you put in previous SOH into input
+    def predict(self, inputX):
+        raise NotImplementedError
+
+
 # Defined the training loop
 if __name__ == "__main__":
-    df_train, _, _ = helper.get_dataset("data/battery_log_processed.csv")
+    torch.manual_seed(1)
+    df_train, _, _ = helper.get_dataset("data/training/battery_log_processed.csv")
     df_train_loader = helper.get_loaders(df_train)
 
     LSTM_model = LSTM(len(list(df_train.columns.difference(["SOH[%]"]))))
-    helper.trainer(df_train_loader, LSTM_model, "loss1.csv")
+    helper.trainer(df_train_loader, LSTM_model, "data/losses/loss1.csv")
